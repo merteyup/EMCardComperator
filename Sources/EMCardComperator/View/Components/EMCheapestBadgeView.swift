50e6220dbcd9b8d7dccfb1e56afe5f9da69f388f
mert.eyup <mert.eyup@infoshop.com.tr>
2025-04-14 00:28:21 +0300

    Improve `EMCheapestBadgeView` with small animation. With this change it seems different than link button and have smooth animation which can take extra attention.

--- Sources/EMCardComperator/View/Components/EMCheapestBadgeView.swift
+++ Sources/EMCardComperator/View/Components/EMCheapestBadgeView.swift
@@ -7,22 +7,35 @@
 
 import SwiftUI
 
-struct CheapestBadgeView: View {
+struct EMCheapestBadgeView: View {
     let isFree: Bool
+    @State private var animateGradient = false
 
     var body: some View {
         let lines = isFree ? ["Kart Aidatı", "Yok"] : ["En", "Uygunu"]
 
-        VStack {
+        VStack(spacing: 2) {
             ForEach(lines, id: \.self) { line in
                 Text(line)
                     .badgeStyle()
             }
         }
-        .padding(.horizontal, 6)
-        .padding(.vertical, 2)
-        .background(.green)
-        .cornerRadius(6)
+        .padding(.horizontal, 8)
+        .padding(.vertical, 4)
+        .background(
+            LinearGradient(
+                gradient: Gradient(colors: [Color.cyan, Color.mint]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .hueRotation(.degrees(animateGradient ? 90 : 0))
+            .animation(.linear(duration: 10).repeatForever(autoreverses: true), value: animateGradient)
+        )
+        .cornerRadius(8)
+        .scaleEffect(animateGradient ? 1.02 : 1)
+        .onAppear {
+            animateGradient = true
+        }
     }
 }
 
